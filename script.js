4807ec6980fd3e35ad6414332211cba785895308
rafael-alves-teixeira <rafael.info.tc@gmail.com>
2022-10-10 15:54:37 -0300

    Background-image, button

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+function testar() {
+    alert('Bem vindo ao jogo da advinhação!!!\nTente advinhar qual número entre 1 e 10 o computador pensou.\nSerá que você consegue acertar?')
+    let numero = Math.floor(Math.random() * (10 - 1 + 1) + 1)
+    let chute = prompt("Qual o valor que você deseja chutar?")
 
-alert('Bem vindo ao jogo da advinhação!!!\nTente advinhar qual número entre 1 e 10 o computador pensou.\nSerá que você consegue acertar?')
-let numero = Math.floor(Math.random() * (10 - 1 + 1) + 1)
-let chute = prompt("Qual o valor que você deseja chutar?")
+    let i = 2
 
-let i = 2
+    while (i != 0) {
+        i--
+        if (numero == chute) {
+            alert("Parabéns! Você é sensacional!!!")
+            break
+        } else {
 
-while (i != 0) {
-    i--
-    if (numero == chute) {
-        alert("Parabéns! Você é sensacional!!!")
-        break
-    } else {    
-        
-        chute = prompt(`Você errou! 🤔 Agora você tem mais ${i + 1} tentativa(s).\n Qual o seu chute?`);
+            chute = prompt(`Você errou! 🤔 Agora você tem mais ${i + 1} tentativa(s).\n Qual o seu chute?`);
+        }
     }
-}
 
-alert(`O computador pensou no número ${numero}.\n Fim do jogo!`)
+    alert(`O computador pensou no número ${numero}.\n Fim do jogo!`)
+}
 
 
